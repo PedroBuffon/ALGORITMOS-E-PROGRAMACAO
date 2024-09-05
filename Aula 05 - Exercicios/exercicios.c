b330f69b619fc55fb65b86fe1f455a5c79d16ee9
Pedro Buffon <henriquebuffon@gmail.com>
2024-09-05 20:59:36 -0300

    update

--- Aula 05 - Exercicios/exercicios.c
+++ Aula 05 - Exercicios/exercicios.c
@@ -1,22 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int primeiroExercicio()
+{
+    int primeiroNumero, segundoNumero;
+
+    printf("Digite o primeiro numero :");
+    scanf("%d" , &primeiroNumero);
+
+    printf("Digite o segundo numero :");
+    scanf("%d" , &segundoNumero);
+
+    if (primeiroNumero > segundoNumero)
+    {
+        printf("O segundo numero deve ser maior que o primeiro");
+    }else
+    {
+        while (primeiroNumero != segundoNumero)
+        {
+            printf("Numero: %d\n", primeiroNumero +=1);
+        }
+    }
+}
+
+int segundoExercicio()
+{
+    int primeiroNumero, segundoNumero;
+
+    printf("Digite o primeiro numero :");
+    scanf("%d" , &primeiroNumero);
+
+    printf("Digite o segundo numero :");
+    scanf("%d" , &segundoNumero);
+
+    if (primeiroNumero > segundoNumero)
+    {
+        printf("O segundo numero deve ser maior que o primeiro");
+    }else
+    {
+        do
+        {
+            printf("Numero: %d\n", primeiroNumero +=1);
+        } while (primeiroNumero != segundoNumero);
+    }
+}
+
+int terceiroExercicio()
+{
+    int primeiroNumero, segundoNumero;
+
+    printf("Digite o primeiro numero :");
+    scanf("%d" , &primeiroNumero);
+
+    printf("Digite o segundo numero :");
+    scanf("%d" , &segundoNumero);
+    
+    if (primeiroNumero > segundoNumero)
+    {
+        printf("O segundo numero deve ser maior que o primeiro");
+    }else
+    {
+        for (int i = primeiroNumero; i <= segundoNumero; i++)
+        {
+            printf("Numero: %d\n", i);
+        }
+    }
+}
+
+int quartoExercicio()
+{
+    int primeiroNumero, segundoNumero, resultado, funcao;
+
+    printf("Digite o primeiro numero :");
+    scanf("%d" , &primeiroNumero);
+
+    printf("Digite o segundo numero :");
+    scanf("%d" , &segundoNumero);
+
+    printf("Selecione a funcao \n 1) +\n 2) -\n 3) *\n 4) / \n");
+    scanf("%d" , &funcao);
+
+    switch (funcao)
+    {
+    case 1:
+        resultado = primeiroNumero + segundoNumero;
+        printf("Resultado: %d\n", resultado);
+        break;
+
+    case 2:
+        resultado = primeiroNumero - segundoNumero;
+        printf("Resultado: %d\n", resultado);
+        break;
+    
+    case 3:
+        resultado = primeiroNumero * segundoNumero;
+        printf("Resultado: %d\n", resultado);
+        break;
+
+    case 4:
+        resultado = primeiroNumero / segundoNumero;
+        printf("Resultado: %d\n", resultado);
+        break;
+
+    default:
+        break;
+    }
+}
 
 int main()
 {
-    int numero;
+    int exercicio;
 
-    numero = 0;
+    printf("Digite o numero do exercicio :");
+    scanf("%d" , &exercicio);
 
-    // while (numero <= 10)
-    // {
-    //     printf("Numero: %d\n", numero);
-    //     numero += 1;
-    // }
+    switch (exercicio)
+    {
+    case 1:
+        primeiroExercicio();
+        break;
 
-    // do
-    // {
-    //     printf("Numero: %d\n", numero);
-    //     numero += 1;
-    // } while (numero <= 10);
+    case 2:
+        segundoExercicio();
+        break;
+    
+    case 3:
+        terceiroExercicio();
+        break;
+
+    case 4:
+        quartoExercicio();
+        break;
+
+    default:
+        break;
+    }
 }
