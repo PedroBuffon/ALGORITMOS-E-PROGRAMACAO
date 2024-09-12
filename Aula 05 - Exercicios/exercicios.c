75091a359081bdec4aa2e3af9d75306cf60d5cbd
Pedro Buffon <henriquebuffon@gmail.com>
2024-09-12 16:28:23 -0300

    terminado aula5

--- Aula 05 - Exercicios/exercicios.c
+++ Aula 05 - Exercicios/exercicios.c
@@ -67,44 +67,56 @@ int terceiroExercicio()
     }
 }
 
-int quartoExercicio()
-{
+int quartoExercicio() {
     int primeiroNumero, segundoNumero, resultado, funcao;
+    char continuar = 'S';
 
-    printf("Digite o primeiro numero :");
-    scanf("%d" , &primeiroNumero);
+    while (continuar == 'S' || continuar == 's') {
+        printf("Digite o primeiro numero: ");
+        scanf("%d", &primeiroNumero);
 
-    printf("Digite o segundo numero :");
-    scanf("%d" , &segundoNumero);
+        printf("Digite o segundo numero: ");
+        scanf("%d", &segundoNumero);
 
-    printf("Selecione a funcao \n 1) +\n 2) -\n 3) *\n 4) / \n");
-    scanf("%d" , &funcao);
+        printf("Selecione a funcao\n 1) +\n 2) -\n 3) *\n 4) /\n");
+        scanf("%d", &funcao);
 
-    switch (funcao)
-    {
-    case 1:
-        resultado = primeiroNumero + segundoNumero;
-        printf("Resultado: %d\n", resultado);
-        break;
+        switch (funcao) {
+            case 1:
+                resultado = primeiroNumero + segundoNumero;
+                printf("Resultado: %d\n", resultado);
+                break;
 
-    case 2:
-        resultado = primeiroNumero - segundoNumero;
-        printf("Resultado: %d\n", resultado);
-        break;
-    
-    case 3:
-        resultado = primeiroNumero * segundoNumero;
-        printf("Resultado: %d\n", resultado);
-        break;
+            case 2:
+                resultado = primeiroNumero - segundoNumero;
+                printf("Resultado: %d\n", resultado);
+                break;
 
-    case 4:
-        resultado = primeiroNumero / segundoNumero;
-        printf("Resultado: %d\n", resultado);
-        break;
+            case 3:
+                resultado = primeiroNumero * segundoNumero;
+                printf("Resultado: %d\n", resultado);
+                break;
 
-    default:
-        break;
+            case 4:
+                if (segundoNumero != 0) {
+                    resultado = primeiroNumero / segundoNumero;
+                    printf("Resultado: %d\n", resultado);
+                } else {
+                    printf("Erro: Divisao por zero nao permitida.\n");
+                }
+                break;
+
+            default:
+                printf("Operacao invalida.\n");
+                break;
+        }
+
+        printf("Deseja realizar outra operacao? (S/N): ");
+        scanf(" %c", &continuar);
     }
+
+    printf("Programa encerrado.\n");
+    return 0;
 }
 
 int main()
